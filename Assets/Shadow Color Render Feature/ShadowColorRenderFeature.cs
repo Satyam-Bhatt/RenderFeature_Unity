f6192e8393667068b16d11d8c149258398ac7582
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-04-09 22:16:22 +0530

    I need to get serious

--- Assets/Shadow Color Render Feature/ShadowColorRenderFeature.cs
+++ Assets/Shadow Color Render Feature/ShadowColorRenderFeature.cs
@@ -2,16 +2,35 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.RenderGraphModule;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UIElements;
 
 public class ShadowColorRenderFeature : ScriptableRendererFeature
 {
     class ShadowColorPass : ScriptableRenderPass
     {
+        private Material _shadowColorMaterial;
+        private RTHandle _coloredShadowTexture;
+
+        private class PassData
+        {
+            // Add any data needed for your render pass here
+        }
+
         public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
         {
-            
+            const string passName = "Shadow Color Pass";
+
+            using (var builder = renderGraph.AddRasterRenderPass<PassData>(passName, out var passData))
+            {
+
+            }
         }
     }
+
+    RenderingData renderingData; // passed into Execute()
+                                 // The shadow map is already a global texture set by URP:
+                                 // _MainLightShadowmapTexture  (screen-space shadow map after URP resolves it)
+                                 // _ScreenSpaceShadowmapTexture (if Screen Space Shadows feature is active)
 
     ShadowColorPass shadowColorPass;
     RenderPassEvent renderPassEvent_Custom = RenderPassEvent.AfterRenderingOpaques;
