ae039c7b78b1f4c5c9d40be5a293e916ca220270
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-04-22 22:10:04 +0530

    Crazy shit happening

---
+++ Assets/CustomLighting/CustomLighting.hlsl
@@ -0,0 +1,15 @@
+#ifndef CUSTOM_LIGHTING_INCLUDED
+#define CUSTOM_LIGHTING_INCLUDED
+
+struct CustomLightingData
+{
+    float3 albedo;
+};
+
+float3 CalculateCustomLighting(CustomLightingData d)
+{
+    
+}
+
+
+#endif
